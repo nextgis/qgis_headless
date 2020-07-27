551ebe663900438d07d246badc805842b7890457
d3n15 <x514ckw4r3@gmail.com>
2020-07-27 23:41:30 +0300

    Added qgis_headless library

---
+++ cmake/FindQGIS_CORE.cmake
@@ -0,0 +1,134 @@
+###############################################################################
+# CMake module to search for PROJ.4 library
+#
+# On success, the macro sets the following variables:
+# QGIS_CORE_FOUND        = if the library found
+# QGIS_CORE_LIBRARIES    = full path to the library
+# QGIS_CORE_INCLUDE_DIRS = where to find the library headers
+# QGIS_CORE_VERSION      = version as defined in qgsconfig.h, as major.minor.patch
+#
+# Copyright (c) 2015 NextGIS <info@nextgis.com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#
+###############################################################################
+
+IF(WIN32)
+
+  SET(OSGEO4W_ROOT "C:/OSGeo4W")
+
+  IF($ENV{OSGEO4W_HOME})
+    SET(OSGEO4W_ROOT "$ENV{OSGEO4W_HOME}")
+  ENDIF()
+
+  IF($ENV{OSGEO4W_ROOT})
+    SET(OSGEO4W_ROOT "$ENV{OSGEO4W_ROOT}")
+  ENDIF()
+
+  # OSGEO4W_QGIS_SUBDIR relative install: qgis[-rel|-ltr][-dev], etc.
+  IF (NOT OSGEO4W_QGIS_SUBDIR OR "${OSGEO4W_QGIS_SUBDIR}" STREQUAL "")
+    IF (NOT "$ENV{OSGEO4W_QGIS_SUBDIR}" STREQUAL "")
+      SET (OSGEO4W_QGIS_SUBDIR $ENV{OSGEO4W_QGIS_SUBDIR})
+    ELSE ()
+      SET (OSGEO4W_QGIS_SUBDIR qgis)
+    ENDIF ()
+  ENDIF ()
+
+  #MESSAGE("Searching for QGIS in $ENV{PROGRAMFILES}/QGIS")
+  IF (MINGW)
+    FIND_PATH(QGIS_CORE_INCLUDE_DIRS
+      NAMES qgsapplication.h
+      PATHS
+        "$ENV{PROGRAMFILES}/QGIS/include"
+    )
+    FIND_LIBRARY(QGIS_CORE_LIBRARIES
+      NAMES qgis_core
+      PATHS
+        "$ENV{PROGRAMFILES}/QGIS/"
+    )
+  ENDIF (MINGW)
+
+  IF (MSVC)
+    FIND_PATH(QGIS_INCLUDE_DIRS
+      NAMES qgsapplication.h
+      PATHS
+        "$ENV{INCLUDE}"
+        "$ENV{LIB_DIR}/include/qgis"
+        "${OSGEO4W_ROOT}/include"
+        "${OSGEO4W_ROOT}/apps/${OSGEO4W_QGIS_SUBDIR}/include"
+        "$ENV{PROGRAMFILES}/QGIS/include"
+    )
+    FIND_LIBRARY(QGIS_CORE_LIBRARIES
+      NAMES qgis_core
+      PATHS
+        "$ENV{LIB_DIR}/lib/"
+        "$ENV{LIB}"
+        "${OSGEO4W_ROOT}/lib"
+        "${OSGEO4W_ROOT}/apps/${OSGEO4W_QGIS_SUBDIR}/lib"
+        "$ENV{PROGRAMFILES}/QGIS/lib"
+    )
+  ENDIF (MSVC)
+ELSE(WIN32)
+  IF(UNIX)
+    #MESSAGE("Searching for QGIS-Core in /usr/bin; /usr/local/bin")
+    FIND_PATH(QGIS_CORE_INCLUDE_DIRS
+      NAMES qgis.h
+      PATHS
+        ${QGIS_BUILD_PATH}/output/lib/qgis_core.framework/Headers
+        ${QGIS_MAC_PATH}/Frameworks/qgis_core.framework/Headers
+        {QGIS_PREFIX_PATH}/include/qgis
+        /usr/include/qgis
+        /usr/local/include/qgis
+        /Library/Frameworks/qgis_core.framework/Headers
+        "$ENV{LIB_DIR}/include/qgis"
+    )
+    FIND_LIBRARY(QGIS_CORE_LIBRARIES
+      NAMES qgis_core
+      PATHS
+        ${QGIS_BUILD_PATH}/output/lib
+        ${QGIS_MAC_PATH}/Frameworks
+        ${QGIS_MAC_PATH}/lib
+        ${QGIS_PREFIX_PATH}/lib/
+        /usr/lib64
+        /usr/lib
+        /usr/local/lib
+        /Library/Frameworks
+        "$ENV{LIB_DIR}/lib/"
+    )
+  ENDIF(UNIX)
+ENDIF(WIN32)
+
+IF (QGIS_CORE_INCLUDE_DIRS)
+  SET(QGIS_CORE_VERSION QGIS_VERSION-NOTFOUND)
+  FIND_FILE(_qgsconfig_h qgsconfig.h PATHS ${QGIS_CORE_INCLUDE_DIRS})
+  IF (_qgsconfig_h)
+    FILE(READ ${_qgsconfig_h} _qgsconfig)
+    IF (_qgsconfig)
+      # version defined like #define VERSION "2.14.8-Essen"
+      FILE(STRINGS "${_qgsconfig_h}" _qgsversion_str REGEX "^#define VERSION .*$")
+      STRING(REGEX REPLACE "^#define VERSION +\"([0-9]+\\.[0-9]+\\.[0-9]+).*$" "\\1" _qgsversion "${_qgsversion_str}")
+      IF (_qgsversion)
+        SET(QGIS_CORE_VERSION ${_qgsversion})
+      ELSE ()
+        MESSAGE(WARNING "No QGIS core version determined: failed to parse qgsconfig.h")
+      ENDIF ()
+    ELSE()
+      MESSAGE(WARNING "No QGIS core version determined: failed to read qgsconfig.h")
+    ENDIF ()
+  ELSE ()
+    MESSAGE(WARNING "No QGIS core version determined: failed to find qgsconfig.h")
+  ENDIF ()
+ENDIF ()
+
+IF (QGIS_CORE_INCLUDE_DIRS AND QGIS_CORE_LIBRARIES)
+   SET(QGIS_CORE_FOUND TRUE)
+ENDIF ()
+
+IF (QGIS_CORE_FOUND)
+   IF (NOT QGIS_CORE_FIND_QUIETLY)
+     MESSAGE(STATUS "Found QGIS core: ${QGIS_CORE_LIBRARIES}")
+   ENDIF (NOT QGIS_CORE_FIND_QUIETLY)
+ELSE (QGIS_CORE_FOUND)
+  MESSAGE(FATAL_ERROR "Could not find QGIS core")
+ENDIF (QGIS_CORE_FOUND)
---
+++ cmake/util.cmake
@@ -0,0 +1,146 @@
+################################################################################
+# Project:  CMake4GDAL
+# Purpose:  CMake build scripts
+# Author:   Dmitry Baryshnikov, polimax@mail.ru
+################################################################################
+# Copyright (C) 2020, NextGIS <info@nextgis.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+################################################################################
+
+function(check_version major minor patch)
+
+    # parse the version number from gdal_version.h and include in
+    # major, minor and rev parameters
+    set(VERSION_FILE ${CMAKE_CURRENT_SOURCE_DIR}/version.h)
+    file(READ ${VERSION_FILE} VERSION_H_CONTENTS)
+
+    string(REGEX MATCH "QGIS_HEADLESS_LIB_MAJOR_VERSION[ \t]+([0-9]+)"
+      MAJOR_VERSION ${VERSION_H_CONTENTS})
+    string (REGEX MATCH "([0-9]+)"
+      MAJOR_VERSION ${MAJOR_VERSION})
+    string(REGEX MATCH "QGIS_HEADLESS_LIB_MINOR_VERSION[ \t]+([0-9]+)"
+      MINOR_VERSION ${VERSION_H_CONTENTS})
+    string (REGEX MATCH "([0-9]+)"
+      MINOR_VERSION ${MINOR_VERSION})
+    string(REGEX MATCH "QGIS_HEADLESS_LIB_PATCH_NUMBER[ \t]+([0-9]+)"
+        PATCH_NUMBER ${VERSION_H_CONTENTS})
+    string (REGEX MATCH "([0-9]+)"
+        PATCH_NUMBER ${PATCH_NUMBER})
+
+    set(${major} ${MAJOR_VERSION} PARENT_SCOPE)
+    set(${minor} ${MINOR_VERSION} PARENT_SCOPE)
+    set(${patch} ${PATCH_NUMBER} PARENT_SCOPE)
+
+    # Store version string in file for installer needs
+    file(TIMESTAMP ${VERSION_FILE} VERSION_DATETIME "%Y-%m-%d %H:%M:%S" UTC)
+    set(VERSION ${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_NUMBER})
+    get_cpack_filename(${VERSION} PROJECT_CPACK_FILENAME)
+    file(WRITE ${CMAKE_BINARY_DIR}/version.str "${VERSION}\n${VERSION_DATETIME}\n${PROJECT_CPACK_FILENAME}")
+
+endfunction(check_version)
+
+function(report_version name ver)
+    string(ASCII 27 Esc)
+    set(BoldYellow  "${Esc}[1;33m")
+    set(ColourReset "${Esc}[m")
+    message("${BoldYellow}${name} version ${ver}${ColourReset}")
+endfunction()
+
+
+# macro to find packages on the host OS
+macro( find_exthost_package )
+    if(CMAKE_CROSSCOMPILING)
+        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
+        set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER )
+        set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER )
+
+        find_package( ${ARGN} )
+
+        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY )
+        set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
+        set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
+    else()
+        find_package( ${ARGN} )
+    endif()
+endmacro()
+
+
+# macro to find programs on the host OS
+macro( find_exthost_program )
+    if(CMAKE_CROSSCOMPILING)
+        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
+        set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER )
+        set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER )
+
+        find_program( ${ARGN} )
+
+        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY )
+        set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
+        set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
+    else()
+        find_program( ${ARGN} )
+    endif()
+endmacro()
+
+function(get_prefix prefix IS_STATIC)
+  if(IS_STATIC)
+    set(STATIC_PREFIX "static-")
+      if(ANDROID)
+        set(STATIC_PREFIX "${STATIC_PREFIX}android-${ANDROID_ABI}-")
+      elseif(IOS)
+        set(STATIC_PREFIX "${STATIC_PREFIX}ios-${IOS_ARCH}-")
+      endif()
+    endif()
+  set(${prefix} ${STATIC_PREFIX} PARENT_SCOPE)
+endfunction()
+
+
+function(get_cpack_filename ver name)
+    get_compiler_version(COMPILER)
+    
+    if(NOT DEFINED BUILD_STATIC_LIBS)
+      set(BUILD_STATIC_LIBS OFF)
+    endif()
+
+    get_prefix(STATIC_PREFIX ${BUILD_STATIC_LIBS})
+
+    set(${name} ${PACKAGE_NAME}-${ver}-${STATIC_PREFIX}${COMPILER} PARENT_SCOPE)
+endfunction()
+
+function(get_compiler_version ver)
+    ## Limit compiler version to 2 or 1 digits
+    string(REPLACE "." ";" VERSION_LIST ${CMAKE_C_COMPILER_VERSION})
+    list(LENGTH VERSION_LIST VERSION_LIST_LEN)
+    if(VERSION_LIST_LEN GREATER 2 OR VERSION_LIST_LEN EQUAL 2)
+        list(GET VERSION_LIST 0 COMPILER_VERSION_MAJOR)
+        list(GET VERSION_LIST 1 COMPILER_VERSION_MINOR)
+        set(COMPILER ${CMAKE_C_COMPILER_ID}-${COMPILER_VERSION_MAJOR}.${COMPILER_VERSION_MINOR})
+    else()
+        set(COMPILER ${CMAKE_C_COMPILER_ID}-${CMAKE_C_COMPILER_VERSION})
+    endif()
+
+    if(WIN32)
+        if(CMAKE_CL_64)
+            set(COMPILER "${COMPILER}-64bit")
+        endif()
+    endif()
+
+    set(${ver} ${COMPILER} PARENT_SCOPE)
+endfunction()
