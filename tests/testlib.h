85abdb9f54bfcd4b781e25b62cb68dc2022dd7f8
d3n15 <x514ckw4r3@gmail.com>
2020-07-29 06:33:11 +0300

    Added tests

--- tests/testlib.h
+++ tests/testlib.h
@@ -1,4 +1,25 @@
-#pragma once
+/******************************************************************************
+*  Project: NextGIS GIS libraries
+*  Purpose: NextGIS headless renderer
+*  Author:  Denis Ilyin, denis.ilyin@nextgis.com
+*******************************************************************************
+*  Copyright (C) 2020 NextGIS, info@nextgis.ru
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+#ifndef TEST_LIB_H
+#define TEST_LIB_H
 
 #include <QObject>
 
@@ -19,3 +40,5 @@ private:
     int mArgc;
     char **mArgv;
 };
+
+#endif // TEST_LIB_H
