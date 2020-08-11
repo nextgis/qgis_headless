827aa5299664bdf49501b72d5ec917eca26a736f
Aleksandr Dezhin <me@dezhin.net>
2020-08-11 04:06:58 +0300

    Add SVG paths tests

---
+++ qgis_headless/tests/data/zero-marker.qml
@@ -0,0 +1,40 @@
+<!DOCTYPE qgis PUBLIC 'http://mrcc.com/qgis.dtd' 'SYSTEM'>
+<qgis labelsEnabled="0" version="3.10.4-A Coruña" styleCategories="Symbology|Labeling">
+  <renderer-v2 type="singleSymbol" symbollevels="0" forceraster="0" enableorderby="0">
+    <symbols>
+      <symbol type="marker" name="0" alpha="1" force_rhr="0" clip_to_extent="1">
+        <layer class="SvgMarker" enabled="1" locked="0" pass="0">
+          <prop k="angle" v="0"/>
+          <prop k="color" v="255,158,23,255"/>
+          <prop k="fixedAspectRatio" v="0"/>
+          <prop k="horizontal_anchor_point" v="1"/>
+          <prop k="name" v="marker.svg"/>
+          <prop k="offset" v="0,0"/>
+          <prop k="offset_map_unit_scale" v="3x:0,0,0,0,0,0"/>
+          <prop k="offset_unit" v="MM"/>
+          <prop k="outline_color" v="35,35,35,255"/>
+          <prop k="outline_width" v="0"/>
+          <prop k="outline_width_map_unit_scale" v="3x:0,0,0,0,0,0"/>
+          <prop k="outline_width_unit" v="Pixel"/>
+          <prop k="scale_method" v="diameter"/>
+          <prop k="size" v="2"/>
+          <prop k="size_map_unit_scale" v="3x:0,0,0,0,0,0"/>
+          <prop k="size_unit" v="MM"/>
+          <prop k="vertical_anchor_point" v="1"/>
+          <data_defined_properties>
+            <Option type="Map">
+              <Option type="QString" name="name" value=""/>
+              <Option name="properties"/>
+              <Option type="QString" name="type" value="collection"/>
+            </Option>
+          </data_defined_properties>
+        </layer>
+      </symbol>
+    </symbols>
+    <rotation/>
+    <sizescale/>
+  </renderer-v2>
+  <blendMode>0</blendMode>
+  <featureBlendMode>0</featureBlendMode>
+  <layerGeometryType>0</layerGeometryType>
+</qgis>
--- qgis_headless/tests/test_render.py
+++ qgis_headless/tests/test_render.py
@@ -7,10 +7,18 @@ from qgis_headless import MapRequest, CRS, Layer, Style
 from qgis_headless.util import image_stat
 
 
-def render_vector(data, qml, extent, size):
+EXTENT_ONE = (-0.5, -0.5, 0.5, 0.5)
+
+
+def render_vector(data, qml, extent, size, svg_paths=None):
     req = MapRequest()
     req.set_dpi(96)
     req.set_crs(CRS.from_epsg(3857))
+    if svg_paths is not None:
+        req.set_svg_paths(svg_paths)
+
+    if isinstance(size, int):
+        size = (size, int(size * (extent[3] - extent[1]) / (extent[2] - extent[0])))
 
     req.add_layer(
         Layer.from_ogr(str(data)),
@@ -26,10 +34,7 @@ def test_contour(fetch, shared_datadir):
 
     extent = (9757454.0, 6450871.0, 9775498.0, 6465163.0)
 
-    base = 1024
-    size = (base, int(base * (extent[3] - extent[1]) / (extent[2] - extent[0])))
-
-    img = render_vector(data, style, extent, size)
+    img = render_vector(data, style, extent, 1024)
     # img.save('test_contour.png')
 
     stat = image_stat(img)
@@ -70,3 +75,48 @@ def test_legend(fetch, shared_datadir):
     assert 240 < stat.green.mean < 245
     assert 240 < stat.blue.mean < 245
 
+
+def test_marker_simple(fetch, shared_datadir):
+    data = (shared_datadir / 'zero.geojson').read_text()
+    style = (shared_datadir / 'zero-marker.qml').read_text()
+
+    img = render_vector(data, style, EXTENT_ONE, 256, svg_paths=[
+        str(shared_datadir / 'marker-blue'), ])
+    # img.save('test_marker_simple.png')
+
+    stat = image_stat(img)
+
+    assert stat.red.max == stat.green.max == 0, "Unexpected data in red or green channel"
+    assert stat.blue.max == 255, "Blue marker is missing"
+
+
+def test_marker_change(fetch, shared_datadir):
+    data = (shared_datadir / 'zero.geojson').read_text()
+    style = (shared_datadir / 'zero-marker.qml').read_text()
+
+    # Render with blue marker in SVG paths
+    img = render_vector(data, style, EXTENT_ONE, 256, svg_paths=[
+        str(shared_datadir / 'marker-blue'), ])
+    assert image_stat(img).blue.max == 255, "Blue marker is missing"
+
+    # Render with green marker in SVG paths
+    img = render_vector(data, style, EXTENT_ONE, 256, svg_paths=[
+        str(shared_datadir / 'marker-green'), ])
+    assert image_stat(img).green.max == 255, "Green marker is missing"
+
+
+@pytest.mark.xfail(reason="SVG marker cache doesn't work yet!")
+def test_marker_cache(fetch, shared_datadir):
+    data = (shared_datadir / 'zero.geojson').read_text()
+    style = (shared_datadir / 'zero-marker.qml').read_text()
+
+    img = render_vector(data, style, EXTENT_ONE, 256, svg_paths=[
+        str(shared_datadir / 'marker-blue'), ])
+    assert image_stat(img).blue.max == 255, "Blue marker is missing"
+
+    (shared_datadir / 'marker-blue' / 'marker.svg').unlink()
+
+    # Render with green marker in SVG paths
+    img = render_vector(data, style, EXTENT_ONE, 256, svg_paths=[
+        str(shared_datadir / 'marker-blue'), ])
+    assert image_stat(img).blue.max == 255, "Blue marker is missing"
