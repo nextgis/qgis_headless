e55245fc82505be4a9ee13c938695a023cfcf78f
Aleksandr Dezhin <me@dezhin.net>
2020-10-15 00:50:03 +0300

    Add field type tests

---
+++ qgis_headless/tests/data/zero-placeholder.qml
@@ -0,0 +1,42 @@
+<!DOCTYPE qgis PUBLIC 'http://mrcc.com/qgis.dtd' 'SYSTEM'>
+<qgis version="3.10.4-A Coruña" styleCategories="Symbology">
+  <renderer-v2 type="RuleRenderer" forceraster="0" symbollevels="0" enableorderby="0">
+    <rules key="{119c6c1e-e730-4072-b8d7-370b7cdc3c21}">
+      <rule symbol="0" key="{84df1f09-01f9-4425-83d8-fa6cee707b02}" filter="/* {{{ */ FALSE /* }}} */"/>
+    </rules>
+    <symbols>
+      <symbol type="marker" force_rhr="0" name="0" alpha="1" clip_to_extent="1">
+        <layer class="SimpleMarker" locked="0" pass="0" enabled="1">
+          <prop k="angle" v="0"/>
+          <prop k="color" v="255,0,0,255"/>
+          <prop k="horizontal_anchor_point" v="1"/>
+          <prop k="joinstyle" v="bevel"/>
+          <prop k="name" v="circle"/>
+          <prop k="offset" v="0,0"/>
+          <prop k="offset_map_unit_scale" v="3x:0,0,0,0,0,0"/>
+          <prop k="offset_unit" v="MM"/>
+          <prop k="outline_color" v="35,35,35,255"/>
+          <prop k="outline_style" v="no"/>
+          <prop k="outline_width" v="0"/>
+          <prop k="outline_width_map_unit_scale" v="3x:0,0,0,0,0,0"/>
+          <prop k="outline_width_unit" v="MM"/>
+          <prop k="scale_method" v="diameter"/>
+          <prop k="size" v="16"/>
+          <prop k="size_map_unit_scale" v="3x:0,0,0,0,0,0"/>
+          <prop k="size_unit" v="Pixel"/>
+          <prop k="vertical_anchor_point" v="1"/>
+          <data_defined_properties>
+            <Option type="Map">
+              <Option type="QString" value="" name="name"/>
+              <Option name="properties"/>
+              <Option type="QString" value="collection" name="type"/>
+            </Option>
+          </data_defined_properties>
+        </layer>
+      </symbol>
+    </symbols>
+  </renderer-v2>
+  <blendMode>0</blendMode>
+  <featureBlendMode>0</featureBlendMode>
+  <layerGeometryType>0</layerGeometryType>
+</qgis>
--- qgis_headless/tests/test_layer.py
+++ qgis_headless/tests/test_layer.py
@@ -1,4 +1,6 @@
 from binascii import a2b_hex
+from datetime import date, time
+from xml.sax.saxutils import quoteattr
 
 import pytest 
 from qgis_headless import Layer, CRS
@@ -24,7 +26,7 @@ def test_from_data_red_circle(shared_datadir, reset_svg_paths):
             ('f_string', Layer.FT_STRING),
         ), (
             (1, WKB_POINT_00, (1, 0.33, 'foo')),
-            (2, WKB_POINT_11, (1, 0.33, '')),
+            (2, WKB_POINT_11, (1, 0.33, None)),
         )
     )
 
@@ -44,3 +46,31 @@ def test_from_data_linestring(shared_datadir, reset_svg_paths):
             (2, WKB_LINESTRING, empty_tuple),
         )
     )
+
+
+@pytest.mark.parametrize('ftype, fvalue, cond', (
+    pytest.param(Layer.FT_INTEGER, None, "field IS NULL", id='integer null'),
+    pytest.param(Layer.FT_INTEGER, 42, "field = 42", id='integer 42'),
+    pytest.param(Layer.FT_REAL, 0.25, "field = 0.25", id='real'),
+    pytest.param(Layer.FT_STRING, '', "field = ''", id='string empty'),
+    pytest.param(Layer.FT_STRING, '0' * 512, "length(field) = 512", id='string 512'),
+    pytest.param(Layer.FT_DATE, (1917, 11, 7), "field = to_date('1917-11-07')", id='date'),
+    pytest.param(Layer.FT_TIME, (16, 20, 0), "field = to_time('16:20:00')", id='time'),
+    pytest.param(Layer.FT_DATETIME, (1917, 11, 7, 16, 20, 0), "field = to_datetime('1917-11-07 16:20:00')", id='datetime'),
+    pytest.param(Layer.FT_INTEGER64, 2**62, "field = 4611686018427387904", id='integer64 2**62'),
+))
+def test_field_type(ftype, fvalue, cond, shared_datadir, reset_svg_paths):
+    style = (shared_datadir / 'zero-placeholder.qml').read_text()
+    crs = CRS.from_epsg(3857)
+
+    layer = Layer.from_data(
+        Layer.GT_POINT, crs, (('field', ftype), ),
+        ((1, WKB_POINT_00, (fvalue, )), ))
+    
+    style = style.replace('"/* {{{ */ FALSE /* }}} */"', quoteattr(cond))
+
+    img = render_vector(layer, style, EXTENT_ONE, 256)
+    # img.save("test_field_type.png")
+
+    stat = image_stat(img)
+    assert stat.red.max == 255, "Red marker missing"
