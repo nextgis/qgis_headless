43ee51c2103fd0f9f2d3d6af870b7cee3a63c550
Aleksandr Dezhin <me@dezhin.net>
2020-10-17 02:47:56 +0300

    Add more svg tests

---
+++ qgis_headless/tests/data/zero-builtin.qml
@@ -0,0 +1,40 @@
+<!DOCTYPE qgis PUBLIC 'http://mrcc.com/qgis.dtd' 'SYSTEM'>
+<qgis styleCategories="Symbology" version="3.10.4-A Coruña">
+  <renderer-v2 forceraster="0" type="singleSymbol" symbollevels="0" enableorderby="0">
+    <symbols>
+      <symbol name="0" clip_to_extent="1" alpha="1" force_rhr="0" type="marker">
+        <layer enabled="1" class="SvgMarker" pass="0" locked="0">
+          <prop k="angle" v="0"/>
+          <prop k="color" v="255,0,0,255"/>
+          <prop k="fixedAspectRatio" v="0"/>
+          <prop k="horizontal_anchor_point" v="1"/>
+          <prop k="name" v="emergency/amenity=hospital.svg"/>
+          <prop k="offset" v="0,0"/>
+          <prop k="offset_map_unit_scale" v="3x:0,0,0,0,0,0"/>
+          <prop k="offset_unit" v="Pixel"/>
+          <prop k="outline_color" v="255,255,255,255"/>
+          <prop k="outline_width" v="0"/>
+          <prop k="outline_width_map_unit_scale" v="3x:0,0,0,0,0,0"/>
+          <prop k="outline_width_unit" v="Pixel"/>
+          <prop k="scale_method" v="diameter"/>
+          <prop k="size" v="32"/>
+          <prop k="size_map_unit_scale" v="3x:0,0,0,0,0,0"/>
+          <prop k="size_unit" v="Pixel"/>
+          <prop k="vertical_anchor_point" v="1"/>
+          <data_defined_properties>
+            <Option type="Map">
+              <Option name="name" type="QString" value=""/>
+              <Option name="properties"/>
+              <Option name="type" type="QString" value="collection"/>
+            </Option>
+          </data_defined_properties>
+        </layer>
+      </symbol>
+    </symbols>
+    <rotation/>
+    <sizescale/>
+  </renderer-v2>
+  <blendMode>0</blendMode>
+  <featureBlendMode>0</featureBlendMode>
+  <layerGeometryType>0</layerGeometryType>
+</qgis>
--- qgis_headless/tests/test_render.py
+++ qgis_headless/tests/test_render.py
@@ -91,6 +91,19 @@ def test_marker_change(shared_datadir, reset_svg_paths):
     assert image_stat(img).green.max == 255, "Green marker is missing"
 
 
+def test_svg_builtin(shared_datadir, reset_svg_paths):
+    data = shared_datadir / 'zero.geojson'
+    style = (shared_datadir / 'zero-builtin.qml').read_text()
+
+    set_svg_paths(['/usr/share/qgis/svg', ])
+
+    layer = Layer.from_ogr(str(data))
+    style = Style.from_string(style)
+
+    img = render_vector(layer, style, EXTENT_ONE, 256)
+    assert image_stat(img).red.max == 255, "Red marker is missing"
+
+
 def test_svg_resolver(shared_datadir, reset_svg_paths):
     data = shared_datadir / 'zero.geojson'
     style = (shared_datadir / 'zero-marker.qml').read_text()
@@ -122,21 +135,44 @@ def test_svg_resolver(shared_datadir, reset_svg_paths):
     assert stat.red.min == stat.green.min == stat.blue.min == 0, "Black question mark is missing"
 
 
-@pytest.mark.xfail(reason="SVG marker cache doesn't work yet!")
-def test_marker_cache(shared_datadir, reset_svg_paths):
+@pytest.mark.parametrize('mode', (
+    pytest.param('resolver'),
+    pytest.param('paths', marks=pytest.mark.xfail),
+))
+def test_svg_cache(mode, shared_datadir, reset_svg_paths):
     data = shared_datadir / 'zero.geojson'
     style = (shared_datadir / 'zero-marker.qml').read_text()
+    marker = (shared_datadir / 'marker-blue' / 'marker.svg').resolve()
 
-    set_svg_paths([str(shared_datadir / 'marker-blue'), ])
+    svg_resolver = None
+    if mode == 'resolver':
+        svg_resolver = lambda _: str(marker)
+    elif mode == 'paths':
+        set_svg_paths([str(marker.parent), ])
+    else:
+        raise ValueError("Invalid mode")
 
     layer = Layer.from_ogr(str(data))
-    style = Style.from_string(style)
+    style = Style.from_string(style, svg_resolver=svg_resolver)
 
-    img = render_vector(layer, style, EXTENT_ONE, 256)
+    req = MapRequest()
+    req.set_crs(CRS.from_epsg(3857))
+    req.add_layer(layer, style)
+
+    img = Image.open(BytesIO(req.render_image(EXTENT_ONE, (256, 256)).to_bytes()))
     assert image_stat(img).blue.max == 255, "Blue marker is missing"
 
-    (shared_datadir / 'marker-blue' / 'marker.svg').unlink()
+    marker.unlink()  # Remove marker file from directory
 
-    # Render with green marker in SVG paths
-    img = render_vector(layer, style, EXTENT_ONE, 256)
-    assert image_stat(img).blue.max == 255, "Blue marker is missing"
+    # And render again
+    img = Image.open(BytesIO(req.render_image(EXTENT_ONE, (256, 256)).to_bytes()))
+    assert image_stat(img).blue.max == 255, "Marker is missing in same MapRequest"
+
+    # Recreata MapRequest with same Layer and Style
+    req = MapRequest()
+    req.set_crs(CRS.from_epsg(3857))
+    req.add_layer(layer, style)
+
+    # And render again
+    img = Image.open(BytesIO(req.render_image(EXTENT_ONE, (256, 256)).to_bytes()))
+    assert image_stat(img).blue.max == 255, "Marker is missing in new MapRequest"
