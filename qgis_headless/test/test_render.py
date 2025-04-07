9e26af0090489baf872759bc6e1c6ffc38a6c656
Ivan Barsukov <i.barsukov@protonmail.com>
2025-04-07 14:05:50 +0300

    Fixed test_countour

--- qgis_headless/test/test_render.py
+++ qgis_headless/test/test_render.py
@@ -52,7 +52,7 @@ def test_contour(save_img, shared_datadir, reset_svg_paths):
     assert stat.green.max == 255, "Primary lines aren't visible"
     assert stat.blue.max == 255, "Primary lines aren't visible"
 
-    assert 3 < stat.red.mean < 4
+    assert 3 < stat.red.mean < 4.05
     assert 12 < stat.green.mean < 13
     assert 29 < stat.blue.mean < 30
 
