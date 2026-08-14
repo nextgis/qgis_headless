36bc0bf9f841570f985e51ed308fea1c1f9e7194
Eduard Popov <popovfest@gmail.com>
2026-08-14 12:48:59 +1000

    Remove fetch test

--- qgis_headless/test/conftest.py
+++ qgis_headless/test/conftest.py
@@ -1,12 +1,9 @@
 import gc
-import json
-import re
 from pathlib import Path
 from typing import Optional
 
 import _qgis_headless
 import pytest
-import requests
 
 
 def pytest_addoption(parser):
@@ -34,40 +31,6 @@ def setup():
 @pytest.fixture()
 def reset_svg_paths():
     _qgis_headless.set_svg_paths([])
-
-
-@pytest.fixture()
-def fetch(cache):
-    cache_path = cache.makedir("fetch")
-
-    def do(url, mode="binary"):
-        name = re.sub(r"^http[s]?\:\/\/", "", url.lower())
-        name = re.sub(r"[^A-z0-9\_]", "_", name)
-        name = re.sub(r"\_{2,}", "_", name)
-
-        fn = cache_path / name
-        if fn.exists():
-            if mode == "file":
-                return fn
-            with fn.open("rb") as fd:
-                data = fd.read()
-        else:
-            resp = requests.get(url)
-            resp.raise_for_status()
-            data = resp.content
-            with fn.open("wb") as fd:
-                fd.write(data)
-
-        if mode == "binary":
-            return data
-        elif mode == "text":
-            return data.decode("utf-8")
-        elif mode == "json":
-            return json.loads(data)
-        elif mode == "file":
-            return fn
-
-    return do
 
 
 @pytest.fixture()
--- qgis_headless/test/test_fetch.py
+++
@@ -1,4 +0,0 @@
-def test_httpbin(fetch):
-    binary = fetch("http://httpbin.org/bytes/64", mode="binary")
-    assert isinstance(binary, bytes)
-    assert len(binary) == 64
